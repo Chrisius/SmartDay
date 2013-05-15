8ecb65dfb29baf1f9b5e06951993697fe254cdc4
Christian Janssen <christian.janssen1@rwth-aachen.de>
2013-05-15 11:02:07 +0200

    Basic Layout
    Basic Layout and List for Sections next: Implementation of
    onClickHandler for Section Fragment

--- src/com/example/smartday/MainActivity.java
+++ src/com/example/smartday/MainActivity.java
@@ -2,24 +2,15 @@ package com.example.smartday;
 
 import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
 
 public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         //Test
         //Test die zweite
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
     }
     
 }
---
+++ src/com/example/smartday/OptionsFragment.java
@@ -0,0 +1,15 @@
+package com.example.smartday;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class OptionsFragment extends Fragment {
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+		return inflater.inflate(R.layout.option_fragment, container, false);
+	}
+
+}
