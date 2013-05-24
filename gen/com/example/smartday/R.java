4f03a4cfc5c80d52b62b9e80b55491523932dd4f
Christian Janssen <christian.janssen1@rwth-aachen.de>
2013-05-24 16:12:27 +0200

    Added Chart engine  and a sample view in Chart-View

--- gen/com/example/smartday/R.java
+++ gen/com/example/smartday/R.java
@@ -30,13 +30,16 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f090006;
+        public static final int action_settings=0x7f090009;
+        public static final int chart_1=0x7f090005;
+        public static final int chart_2=0x7f090007;
+        public static final int chart_3=0x7f090006;
         public static final int option_header=0x7f090003;
         public static final int options_fragment_container=0x7f090001;
         public static final int options_list=0x7f090004;
         public static final int section_fragment=0x7f090000;
         public static final int section_fragment_container=0x7f090002;
-        public static final int section_list_fragment=0x7f090005;
+        public static final int section_list_fragment=0x7f090008;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
--- src/com/example/smartday/SectionChartFragment.java
+++ src/com/example/smartday/SectionChartFragment.java
@@ -1,14 +1,135 @@
 package com.example.smartday;
 
+import org.achartengine.ChartFactory;
+import org.achartengine.GraphicalView;
+import org.achartengine.model.CategorySeries;
+import org.achartengine.renderer.DefaultRenderer;
+import org.achartengine.renderer.SimpleSeriesRenderer;
+
 import android.app.Fragment;
+//import android.content.Context;
+//import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 public class SectionChartFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
 		return inflater.inflate(R.layout.section_chart_fragment, container, false);
+	}
+	
+	@Override
+	public void onResume(){
+		super.onResume();
+		draw();
+	}
+	
+	private void draw(){
+		draw3();
+		draw1();
+		draw2();
+/*		CategorySeries categories = new CategorySeries("Number1");
+		categories.add("Social", 12.0);
+		categories.add("Productiv", 6.0);
+		categories.add("Undefined", 8);
+		
+		int[] colors = {Color.BLUE, Color.GREEN, Color.RED};
+		
+		DefaultRenderer renderer = new DefaultRenderer();
+		for(int color : colors){
+			SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+			r.setColor(color);
+			renderer.addSeriesRenderer(r);
+		}
+		
+		renderer.setPanEnabled(false);
+		renderer.setInScroll(true);
+		
+		GraphicalView chartView = ChartFactory.getPieChartView(getActivity(), categories, renderer);
+					
+		LinearLayout layout = (LinearLayout) getActivity().findViewById(R.id.chart_1);
+		layout.addView(chartView);
+		
+		chartView = ChartFactory.getPieChartView(getActivity(), categories, renderer);
+		layout = (LinearLayout) getActivity().findViewById(R.id.chart_2);
+		layout.addView(chartView);
+		
+		chartView = ChartFactory.getPieChartView(getActivity(), categories, renderer);
+		layout = (LinearLayout) getActivity().findViewById(R.id.chart_3);
+		layout.addView(chartView);
+*/		
+	}
+	private void draw1(){
+		CategorySeries categories = new CategorySeries("Number1");
+		categories.add("Social", 12.0);
+		categories.add("Productiv", 6.0);
+		categories.add("Undefined", 8);
+		
+		int[] colors = {Color.CYAN, Color.MAGENTA, Color.BLACK};
+		
+		DefaultRenderer renderer = new DefaultRenderer();
+		for(int color : colors){
+			SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+			r.setColor(color);
+			renderer.addSeriesRenderer(r);
+		}
+		
+		renderer.setPanEnabled(false);
+		renderer.setInScroll(true);
+		
+		GraphicalView chartView = ChartFactory.getPieChartView(getActivity(), categories, renderer);
+					
+		LinearLayout layout = (LinearLayout) getActivity().findViewById(R.id.chart_1);
+		layout.addView(chartView);
+	}
+	private void draw2(){
+		CategorySeries categories = new CategorySeries("Number2");
+		categories.add("Social", 12.0);
+		categories.add("Productiv", 6.0);
+		categories.add("Undefined", 8);
+		
+		int[] colors = {Color.BLUE, Color.GREEN, Color.RED};
+		
+		DefaultRenderer renderer = new DefaultRenderer();
+		for(int color : colors){
+			SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+			r.setColor(color);
+			renderer.addSeriesRenderer(r);
+		}
+		
+		renderer.setPanEnabled(false);
+		renderer.setInScroll(true);
+		
+		GraphicalView chartView = ChartFactory.getPieChartView(getActivity(), categories, renderer);
+					
+		LinearLayout layout = (LinearLayout) getActivity().findViewById(R.id.chart_2);
+		layout.addView(chartView);
+	}
+	private void draw3(){
+		CategorySeries categories = new CategorySeries("Number3");
+		categories.add("Social", 12.0);
+		categories.add("Productiv", 6.0);
+		categories.add("Undefined", 8);
+		
+		int[] colors = {Color.LTGRAY, Color.BLUE, Color.YELLOW};
+		
+		DefaultRenderer renderer = new DefaultRenderer();
+		for(int color : colors){
+			SimpleSeriesRenderer r = new SimpleSeriesRenderer();
+			r.setColor(color);
+			renderer.addSeriesRenderer(r);
+		}
+		
+		renderer.setPanEnabled(false);
+		renderer.setInScroll(true);
+		
+		GraphicalView chartView = ChartFactory.getPieChartView(getActivity(), categories, renderer);
+					
+		LinearLayout layout = (LinearLayout) getActivity().findViewById(R.id.chart_3);
+		layout.addView(chartView);
 	}
 }
