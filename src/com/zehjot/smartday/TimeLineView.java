4b4cb9b8f07cf7585de4c7362556b1d2da104b6a
Christian Janssen <christian.janssen1@rwth-aachen.de>
2013-07-19 09:12:56 +0200

    Start of Timeline

---
+++ src/com/zehjot/smartday/TimeLineView.java
@@ -0,0 +1,19 @@
+package com.zehjot.smartday;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.view.View;
+
+public class TimeLineView extends View {
+	public TimeLineView(Context context) {
+		super(context);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void onDraw(Canvas canvas) {
+		// TODO Auto-generated method stub
+		super.onDraw(canvas);
+	}
+	
+}
